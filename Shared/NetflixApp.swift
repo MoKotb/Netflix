62aee52be61630f5e875ce5be0b5857023bb2fd1
Mohamed Kotb <mohamed.qotb@vodafone.com>
2020-12-04 21:38:08 +0200

    Initial Commit

---
+++ Shared/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Shared
+//
+//  Created by Vodafone Egypt on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
